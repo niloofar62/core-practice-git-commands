739f719bebf94a9b29549c623b18db0604cd68f8
Niloofar Nayebi <nil_nyb@yahoo.com>
2022-09-22 14:06:05 -0400

    c

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def always_returns_true():
     print("Hello")
     print ("kat's change #2")
     return True
-print("you are completely correct")
+
 
 def test_always_returns_true():
     assert always_returns_true()
